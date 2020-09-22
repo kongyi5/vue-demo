363b5436737143c4ef6cf5482a22cec77cd7ac02
kongyi <jerrykongyi1997@gmail.com>
2020-09-22 23:41:51 +0800

    add component

---
+++ src/Cell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="cell" v-on:click="a = true">
+      <template v-if="a">{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: false,
+      text: "o",
+    };
+  },
+};
+</script>
+
+<style>
+.cell {
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 68px;
+}
+</style>
