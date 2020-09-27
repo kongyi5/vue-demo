df74fe70543ecb2954388ba79fdb63d475639046
kongyi <jerrykongyi1997@gmail.com>
2020-09-27 23:09:59 +0800

    rebuild 2.0

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-demo-1/dist/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-demo-1/dist" : "/",
 };
